5a160c36aa76aacac23bf5a3b3e734677e293e97
James Long <james.long@paulsmith.co.uk>
2014-07-16 19:27:49 +0100

    Directory fix

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
