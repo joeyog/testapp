97fbe32152529cb3f9a58afc8dbd0d87c228e236
James Long <james.long@paulsmith.co.uk>
2014-07-16 21:01:58 +0100

    Stuff

--- public/index.php
+++ public/index.php
@@ -15,8 +15,21 @@ $app->get('/', function() use($app) {
     return $app['twig']->render('index.html');
 });
 
-$app->get('/{name}', function($name) use($app) {
-    return $app['twig']->render('index.html', array('name' => $name));
+$app->get('/about', function() use($app) {
+    return $app['twig']->render('about.html');
+});
+
+$app->get('/name/{name}/age/{age}', function($name, $age) use($app) {
+
+//    $model = new stdClass();
+//    $model->name = $name;
+//    $model->age = $age;
+
+    require_once __DIR__ . '../src/testapp/TestModel.php';
+
+    $myModel = new \testapp\TestModel($name, $age);
+
+    return $app['twig']->render('test.html', array('model' => $myModel));
 });
 
 $app->run();
---
+++ src/testapp/TestModel.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jlong
+ * Date: 16/07/2014
+ * Time: 20:51
+ */
+
+namespace testapp;
+
+/**
+ * Class TestModel
+ * @package testapp
+ */
+class TestModel
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $age;
+
+    /**
+     * @param string $name
+     * @param string $age
+     */
+    public function __construct($name, $age)
+    {
+        $this->age = $age;
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
